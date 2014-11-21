4ac95bac18a0b07f0d045eface632f41bf853cbe
Inspir3 <andice.rupen@gmail.com>
2014-11-21 21:55:28 +0000

    Test de persistance

--- app/depense/application.js
+++ app/depense/application.js
@@ -1,19 +1,26 @@
-var m = angular.module('application', ['persistance']);
+var m = angular.module('application', ['inspir3']);
 
-m.controller('DepenseControleur', ['$scope', 'depense', 'service', function($scope, depense, service) {
+m.controller('DepenseControleur', ['$scope', 'depense', 'persistance', function($scope, depense, persistance) {
     
     $scope.depenses = [ {description: 'aileron', achat: 220, vente: 190, poids: 438},
                         {description: 'flotteur', achat: 700, vente: 500, poids: 7250} ];
- 
+     
+    /*
+     * 
+     */
+    var sauver = function(){
+        persistance.sauvegarder('data', angular.toJson($scope.depenses));         
+    }
+    
     /*
      *  
      */
     $scope.ajouterDepense = function() {
-      console.log('Ajout 7 !');
+        console.log('ajouterDepense()');
+             
+        $scope.depenses.push(depense.creer($scope.description, $scope.achat, $scope.vente, $scope.poids));
         
-      //service.sauvegarder('fichier1', 'data1');
-      
-      $scope.depenses.push(depense.creer($scope.description, $scope.achat, $scope.vente, $scope.poids));
+        sauver();
     }
     
 }]);
